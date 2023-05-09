035b81c54f2d8eb023bbb65541464c7552a6ab3c
Inthania Nadicika Kurniawan <nadacika17@gmail.com>
2023-05-09 09:55:15 +0700

    tampilan user - tentang kami

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,7 +18,7 @@
 </head>
 <body>
   <!-- Navbar -->
-  <section id="navbar">
+  <section id="beranda">
     <nav class="navbar">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
@@ -26,7 +26,10 @@
           </a>
           <ul>
             <li class="nav-item">
-              <a class="active" href="#">Beranda</a>
+              <a class="active" href="#beranda">Beranda</a>
+            </li>
+            <li class="nav-item">
+              <a href="#tentang-kami">Tentang Kami</a>
             </li>
             <li class="nav-item">
               <a href="#">Layanan</a>
@@ -39,9 +42,41 @@
             </li>
           </ul>
         </div>
-      </nav>
+    </nav>
     <!-- Carousel -->
+    <div id="hero-carousel" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#hero-carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#hero-carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#hero-carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active c-item">
+          <img src="assets/dist/img/slide3.jpg" class="d-block w-100 c-img" alt="...">
+        </div>
+        <div class="carousel-item c-item">
+          <img src="assets/dist/img/slide2.jpg" class="d-block w-100 c-img" alt="...">
+        </div>
+        <div class="carousel-item c-item">
+          <img src="assets/dist/img/slide1.jpg" class="d-block w-100 c-img" alt="...">
+        </div>
+      </div>
+    </div>
   </section>
+
+  <!--Tentang Kami-->
+  <section id="tentang-kami">
+    <div class="judul">
+      <h3>Tentang Kami</h3>
+      <span id="garis"></span>
+    </div>
+    <div class="isi">
+      <img src="assets/dist/img/foto1.jpg" alt="">
+      <p>Bank Sampah Malang (BSM) adalah suatu lembaga yang berbadan hukum koperasi yang pendiriannya difasilitasi oleh Pemerintah Kota Malang melalui Dinas Kebersihan dan Pertamanan untuk membantu dalam hal pemberdayaan masyarakat untuk ikut serta aktif dalam pengolahan sampah dari sumbernya (rumah tangga). Seiring perjalanan waktu BSM menjadi mitra Kota Malang dalam hal membina, melatih, mendampingi dalam pengolahan sampah 3R (reduce, reuse and recycle) masyarakat Kota Malang yang mandiri.</p>
+    </div>
+
+  </section>
+
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
