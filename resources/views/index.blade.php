61151fcb441249e345b46e48cc11f8ae30464903
Inthania Nadicika Kurniawan <nadacika17@gmail.com>
2023-05-09 07:45:32 +0700

    coba slider

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
 
+    <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     
 </head>
 <body>
@@ -18,23 +24,24 @@
           <a class="navbar-brand" href="#">
             <img src="assets/dist/img/logo.png" alt="logo" width="80">
           </a>
-          <ul class="nav justify-content-end nav-underline">
+          <ul>
             <li class="nav-item">
-              <a class="nav-link active text-reset" href="#">Beranda</a>
+              <a class="active" href="#">Beranda</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link text-reset" href="#">Layanan</a>
+              <a href="#">Layanan</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Sampah</a>
+              <a href="#">Sampah</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Temukan Kami</a>
+              <a href="#">Temukan Kami</a>
             </li>
           </ul>
         </div>
       </nav>
-    </section>
+    <!-- Carousel -->
+  </section>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
