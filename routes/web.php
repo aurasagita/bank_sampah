47dbe246ad4ed4fbfc7694180e1c954edca03433
tesyaeriana <2141720024@student.polinema.ac.id>
2023-05-11 21:40:05 +0700

    edit_route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\JadwalController;
 use App\Http\Controllers\NasabahController;
+use App\Http\Controllers\SampahController;
 use App\Http\Controllers\SopirController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,6 +28,7 @@ Route::get('/index', [IndexController::class, 'index']);
 Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::resource('/jadwal',JadwalController::class)->parameter('jadwal','id');
 Route::resource('/nasabah', NasabahController::class)->parameter('nasabah', 'id');
+Route::resource('/sampah', SampahController::class)->parameter('sampah', 'id');
 Route::get('/sopir', [SopirController::class, 'index']);
 
 
