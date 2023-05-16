b42fdee64a2581406bcc170a3bb4fa1dbb875525
Inthania Nadicika Kurniawan <nadacika17@gmail.com>
2023-05-16 11:37:11 +0700

    fix login page

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -1,84 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Login Bank Sampah Malang</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login Bank Sampah</title>
 
+  <!-- CSS -->
+  <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+  <!-- Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   <!-- Google Font -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css')}}">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="{{ asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('assets/dist/css/adminlte.min.css')}}">
+  <script src="https://kit.fontawesome.com/95c066a903.js" crossorigin="anonymous"></script>
+
 </head>
-<body class="hold-transition login-page">
-<div class="login-box">
-  <div class="login-logo">
-    <a href="/index2.html"><b>Login </b>Bank Sampah Malang</a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="card">
-    <div class="card-body login-card-body">
-      
-      <p class="login-box-msg">Sign in to start your session</p>
-
-      <form action="{{url('/login')}}" method="post">
-        @csrf
-        <div class="input-group mb-3">
-          <input name="email" type="text" class="form-control" placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+<body>
+  <section class="login d-flex">
+    <div class="login-left w-50">
+      <div class="row justify-content-center align-items-center h-100">
+        <div class="col-6">
+          <div class="header">
+            <h1>Login Bank Sampah</h1>
+          </div>
+            <div class="login-form">
+              <form action="{{url('/login')}}" method="post">
+                @csrf
+                <label for="email" class="form-label">Email</label>
+                <input name="email" type="text" class="form-control" placeholder="Enter your email">
+                <label for="password" class="form-label">Password</label>
+                <input name="password" type="password" class="form-control" placeholder="Enter your password">
+                <button type="submit" class="signin">Sign In</button>
+                <div class="text-center">
+                  <span class="d-inline">Don't have an account? <a href="{{url('/register')}}" class="d-inline text-decoration-none">Register</a></span>
+                </div>
             </div>
-          </div>
         </div>
-        <div class="input-group mb-3">
-          <input name="password" type="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-
-      <!-- /.social-auth-links -->
-
-      
-      <p class="mb-0">
-        <a href="{{url('/register')}}" class="text-center">Register a new admin</a>
-      </p>
+      </div>
     </div>
-    <!-- /.login-card-body -->
-  </div>
-</div>
-<!-- /.login-box -->
-
-<!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
+    <div class="login-right">
+      <div class="row justify-content-center align-items-center h-100">
+        <img src="assets/dist/img/login3.png" alt="">
+      </div>
+    </div>
+  </section> 
 </body>
 </html>
