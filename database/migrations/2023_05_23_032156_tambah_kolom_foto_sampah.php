43a49d241b9e86c0bd0aa84fa5bc2ab202db8732
SALMA ANNISSA AZIZI <salmaannisaazizi@gmail.com>
2023-05-24 12:52:26 +0700

    migrasi tambah kolom gambar di sampah

--- database/migrations/2023_05_23_032156_tambah_kolom_foto_sampah.php
+++ database/migrations/2023_05_23_032156_tambah_kolom_foto_sampah.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,9 +14,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('sopir',function(Blueprint $table){
-            $table->string('email')->unique()->after('nama');
-            $table->string('password');
+        Schema::table('sampah',function(Blueprint $table){
+            $table->string('gambar')->after('jenis_sampah');
         });
     }
 
