80b0cbd649f21dedf7682cf36b318ed0d0a38fd0
tesyaeriana <2141720024@student.polinema.ac.id>
2023-05-28 21:53:11 +0700

    modifikasi_controller

--- app/Http/Controllers/PageNasabahController.php
+++ app/Http/Controllers/PageNasabahController.php
@@ -4,18 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\JadwalModel;
 use App\Models\NasabahModel;
-<<<<<<< HEAD
 use App\Models\SampahModel;
 use App\Models\SopirModel;
 use App\Models\TransaksiBaruModel;
 use App\Models\TransaksiModel;
-=======
-use App\Models\User;
->>>>>>> 4829750cd37d20552c6e1e29d0452a21b93bf78d
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Storage;
 
 class PageNasabahController extends Controller
 {
@@ -55,7 +49,6 @@ class PageNasabahController extends Controller
      */
     public function store(Request $request)
     {
-<<<<<<< HEAD
         for($i = 0; $i < $request->counter; $i++){
             $berat = $request['berat_'.$i];
             $jenisSampah = SampahModel::find($request['jenis_sampah_'.$i]);
@@ -72,52 +65,6 @@ class PageNasabahController extends Controller
         
 
         //$data = JadwalModel::create($request->except(['_token']));
-=======
-        $request->validate([
-            'id_nasabah'=>'required',
-            'id_sopir'=>'required',
-            'tanggal_pengambilan'=>'required|date',
-            'konfirmasi'=>'required|string',
-            'nama'=>'required|string|max:50',
-            'foto' => 'required|image|mimes:jpeg,png,jpg|max:2048',
-            'alamat'=>'required|string|max:255',
-            'phone'=>'required|digits_between:5, 15',
-            'email'=>'required|string|unique:users,email',
-            'password' => 'required|string|min:4'
-        ]);
-
-        if ($request->file('foto')) {
-            $file = $request->file('foto');
-            $extension = $file->getClientOriginalExtension();
-            $filename = 'sopir-' . 'nama' . '.' . $extension;
-            $image_name = $file->storeAs('sopirprofile', $filename, 'public');
-        }
-
-        User::create([
-            'name' => $request->input('nama'),
-            'email' => $request->input('email'),
-            'password' => Hash::make($request->input('password')),
-            'role' => 'nasabah',
-        ]);
-
-        NasabahModel::create([
-            'id_nasabah' => $request->input('id_nasabah'),
-            'nama' => $request->input('nama'),
-            'foto' => $image_name,
-            'alamat' => $request->input('alamat'),
-            'phone' => $request->input('phone'),
-            'email' => $request->input('email'),
-            'password' => Hash::make($request->input('password')),
-        ]);
-
-        JadwalModel::create([
-            'id_nasabah'=> $request->input('id_nasabah'),
-            'id_sopir'=> $request->input('id_sopir'),
-            'tanggal_pengambilan'=> $request->input('tanggal_pengambilan'),
-            'konfirmasi'=> $request->input('konfirmasi'),
-        ]);
-
->>>>>>> 4829750cd37d20552c6e1e29d0452a21b93bf78d
         return redirect('jadwalnasabah')
             ->with('success', 'Jadwal Berhasil Ditambahkan');
     }
@@ -130,15 +77,8 @@ class PageNasabahController extends Controller
      */
     public function show($id)
     {
-<<<<<<< HEAD
         $jadwal = TransaksiBaruModel::where('id', $id)->get();
         return view('pagenasabah.detail_jadwal', ['jadwal' => $jadwal[0]]);
-=======
-        $user = Auth::user();
-        $jadwal = JadwalModel::where('id', $id)->get();
-        $nasabah = NasabahModel::where('email', $user->email)->first();
-        return view('pagenasabah.detail_jadwal', compact('nasabah'), ['jadwal' => $jadwal[0]]);
->>>>>>> 4829750cd37d20552c6e1e29d0452a21b93bf78d
     }
 
     /**
@@ -149,13 +89,7 @@ class PageNasabahController extends Controller
      */
     public function edit($id)
     {
-        $nasabah = NasabahModel::find($id);
-        return view('layouts.aaa')
-        ->with('nasabah', $nasabah)->with('url_form', url('/pagenasabah/'.$id));
-        // $user = Auth::user();
-        // //$jadwalUser = JadwalModel::where('id_nasabah', $user->id)->get();
-        // $nasabah = NasabahModel::where('email', $user->email)->first();
-        // return view('pagenasabah.navbar', compact('nasabah'));
+        //
     }
 
     /**
@@ -167,50 +101,7 @@ class PageNasabahController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $request->validate([
-            'id_nasabah'=>'required|string|max:10|unique:nasabah,id_nasabah,'.$id,
-            'nama'=>'required|string|max:50',
-            'foto' => 'image|mimes:jpeg,png,jpg|max:2048',
-            'alamat'=>'required|string|max:255',
-            'phone'=>'required|digits_between:5, 15',
-        ]);
-
-        $nasabah = NasabahModel::find($id);
-
-        if ($request->hasFile('foto')) {
-            $foto = $request->file('foto');
-            $fotoName = 'nasabahprofile/'.'nasabah-' . $nasabah->nama . '.' . $foto->getClientOriginalExtension();
-            !is_null($nasabah->foto) && Storage::delete($nasabah->foto);
-
-            $foto->storeAs('nasabahprofile', $fotoName, 'public');
-            $nasabah->foto = $fotoName;
-        }
-        
-        NasabahModel::where('id', $id)->update([
-            'id_nasabah' => $request->id_nasabah,
-            'nama' => $request->nama,
-            'alamat' => $request->alamat,
-            'phone' => $request->phone,
-            'email' => $request->email
-        ]);
-
-        $nsb = User::where('email', $nasabah->email)->first();
-        
-        User::where('email', $nsb->email)->update([
-            'name' => $request->nama,
-            'email' => $request->email,
-        ]);  
-        
-        $nasabah->save();
-
-        if ($request->filled('password')) {
-            $user = User::where('email', $nasabah->email)->first();
-            $nasabah->password = Hash::make($request->password);
-            $user->save();
-        }
-
-        return redirect('nasabah')
-            ->with('success', 'Nasabah Berhasil Ditambahkan');
+        //
     }
 
     /**
