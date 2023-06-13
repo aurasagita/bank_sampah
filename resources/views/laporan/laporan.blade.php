0c13e5a72de1e5b46c4a912679579c7347a7445f
tesyaeriana <2141720024@student.polinema.ac.id>
2023-06-14 00:07:01 +0700

    fix_cetak laporan domPDF

--- resources/views/laporan/laporan.blade.php
+++ resources/views/laporan/laporan.blade.php
@@ -6,20 +6,27 @@
     {{Breadcrumbs::render('transaksi')}}
   </div>
   <div class="form-group text-left"><a href="{{url('/grafik_penjualan/')}}" class="btn btn-sm btn-success">Lihat Grafik</a></div>
-<div class="card">
-  <div class="card-body">
-    <div class="input-group mb-3 mt-2">
-     <label class="mb-3">Tanggal Awal <input type="date" name="tanggal_awal" id="tanggal_awal" class="form-control"></label></div>
-    <div class="input-group mb-3">
-     <label for="label">Tanggal Akhir<input type="date" name="tanggal_akhir" id="tanggal_akhir" class="form-control"></label></div>
-   </div>
- </div>
- <div class="input-group mb-3">
-  <a href="" class="btn btn-sm btn-primary" onclick="this.href='cetakTanggal/'+ document.getElementById('tanggal_awal').value + '/' + document.getElementById('tanggal_akhir').value " target="_blank">Cetak</a>
-   </div>
 
- 
-    
+
+ <div class="card">
+  <div class="card-body">
+    <form action="{{ route('laporan.cetak') }}" method="POST">
+      @csrf
+      <div class="input-group mb-3 mt-2">
+        <label style="padding-right: 30px" class="mb-3" for="tanggal_awal">Tanggal Awal:</label>
+        <input class="form-control" type="date" id="tanggal_awal" name="tanggal_awal">
+      </div>
+      <div class="input-group mb-3 mt-2">
+        <label style="padding-right: 27px" class="mb-3" for="tanggal_akhir">Tanggal Akhir:</label>
+        <input class="form-control" type="date" id="tanggal_akhir" name="tanggal_akhir">
+      </div>
+      <button class="btn btn-sm btn-primary btn-block" type="submit">Cetak Laporan PDF    <i class="fa fa-print" aria-hidden="true"></i></button>
+  </form>
+  
+  </div>
+
+ </div>
+  
 </section>
     
 @endsection
