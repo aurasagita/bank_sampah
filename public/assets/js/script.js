f98c3e0ade2c60d77d2d24ac61b80bdcd0c53b3d
Inthania Nadicika Kurniawan <nadacika17@gmail.com>
2023-06-15 00:37:05 +0700

    add modal preview jenis sampah

---
+++ public/assets/js/script.js
@@ -0,0 +1,22 @@
+let previewContainer = document.querySelector('.sampah-preview');
+let previewBox = previewContainer.querySelectorAll('.preview')
+
+document.querySelectorAll('.sampah .box').forEach(box =>{
+    box.onclick = () =>{
+        previewContainer.style.display = 'flex';
+        let name = box.getAttribute('data-name');
+        previewBox.forEach(preview =>{
+            let target = preview.getAttribute('data-target');
+            if (name == target) {
+                preview.classList.add('active');
+            }
+        });
+    };
+});
+
+previewBox.forEach(close =>{
+    close.querySelector('.fa-times').onclick = () =>{
+        close.classList.remove('active');
+        previewContainer.style.display = 'none';
+    };
+});
